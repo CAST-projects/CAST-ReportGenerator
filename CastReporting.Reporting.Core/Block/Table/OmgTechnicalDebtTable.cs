0c24cd72d116b7c8f9a02d26f486727bef5af7e2
Aurore Eteve <a.eteve@castsoftware.com>
2021-10-19 14:54:39 +0200

    REPORTGEN-1050: New application table component OMG_TECHNICAL_DEBT_TABLE

---
+++ CastReporting.Reporting.Core/Block/Table/OmgTechnicalDebtTable.cs
@@ -0,0 +1,91 @@
+﻿
+/*
+ *   Copyright (c) 2019 CAST
+ *
+ * Licensed under a custom license, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License, accessible in the main project
+ * source code: Empowerment.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using CastReporting.BLL.Computing;
+using CastReporting.Domain;
+using CastReporting.Reporting.Atrributes;
+using CastReporting.Reporting.Builder.BlockProcessing;
+using CastReporting.Reporting.Core.Languages;
+using CastReporting.Reporting.Helper;
+using CastReporting.Reporting.ReportingModel;
+using System.Collections.Generic;
+
+
+namespace CastReporting.Reporting.Block.Table
+{
+    [Block("OMG_TECHNICAL_DEBT_TABLE")]
+    public class OmgTechnicalDebtTable : TableBlock
+    {
+        public override TableDefinition Content(ReportData reportData, Dictionary<string, string> options)
+        {
+            const string numberFormat = "N1";
+
+            bool displayShortHeader = options.GetOption("HEADER","DAYS").ToUpper().Equals("SHORT");
+            string index = options.GetOption("ID", "ISO");
+            Snapshot snapshot = options.GetOption("SNAPSHOT", "CURRENT").ToUpper().Equals("PREVIOUS") ? reportData.PreviousSnapshot ?? null : reportData.CurrentSnapshot ?? null;
+
+            List<string> rowData = new List<string>();
+            rowData.AddRange(new[] { Labels.Name, Labels.Value });
+
+            if (snapshot != null)
+            {
+                OmgTechnicalDebt omgTechnicalDebt = reportData.SnapshotExplorer.GetOmgTechnicalDebt(reportData.Application.Href, index, snapshot.GetId());
+                //Build Debt row          
+                double? technicalDebtBuild = (double?) omgTechnicalDebt?.Total / 8 / 60 ?? null;
+                rowData.AddRange(new[] {
+                    displayShortHeader ? Labels.Debt : Labels.TechnicalDebt  + " (" + Labels.Days + ")",
+                   technicalDebtBuild?.ToString(numberFormat) ?? Constants.No_Value
+                });
+
+
+                //Build Debt added row            
+                double? technicalDebtadded = (double?) omgTechnicalDebt?.Added / 8 / 60 ?? null;
+                rowData.AddRange(new[] {
+                     displayShortHeader ? Labels.DebtAdded : Labels.TechnicalDebtAdded + " (" + Labels.Days + ")",
+                   technicalDebtadded?.ToString(numberFormat) ?? Constants.No_Value
+                });
+
+                //Build Debt removed row            
+                double? technicalDebtremoved = (double?) omgTechnicalDebt?.Removed / 8 / 60 ?? null;
+                rowData.AddRange(new[] {
+                     displayShortHeader ? Labels.DebtRemoved : Labels.TechnicalDebtRemoved + " (" + Labels.Days + ")",
+                   technicalDebtremoved?.ToString(numberFormat) ?? Constants.No_Value
+                });
+            }
+            if (rowData.Count == 2)
+            {
+                rowData.AddRange(new[] { displayShortHeader ? Labels.Debt : Labels.TechnicalDebt  + " (" + Labels.Days + ")", Constants.No_Value });
+                rowData.AddRange(new[] { displayShortHeader ? Labels.DebtAdded : Labels.TechnicalDebtAdded + " (" + Labels.Days + ")", Constants.No_Value });
+                rowData.AddRange(new[] { displayShortHeader ? Labels.DebtRemoved : Labels.TechnicalDebtRemoved + " (" + Labels.Days + ")", Constants.No_Value });
+
+            }
+
+            //Build Table Definition            
+            var resultTable = new TableDefinition
+            {
+                HasRowHeaders = false,
+                HasColumnHeaders = true,
+                NbRows = 3,
+                NbColumns = 2,
+                Data = rowData
+            };
+
+            return resultTable;
+        }
+
+    }
+}
---
+++ CastReporting.UnitTest.Core/Reporting/Tables/OmgTechnicalDebtTableTests.cs
@@ -0,0 +1,120 @@
+﻿using CastReporting.Domain;
+using CastReporting.Reporting.Block.Table;
+using CastReporting.Reporting.ReportingModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace CastReporting.UnitTest.Reporting.Tables
+{
+    [TestClass]
+    public class OmgTechnicalDebtTableTests
+    {
+        [TestInitialize()]
+        public void Initialize()
+        {
+            TestUtility.SetCulture("en-US");
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\Data\CurrentBCresults.json", "Data")]
+        public void TestContent()
+        {
+            CastDate currentDate = new CastDate { Time = 1484953200000 };
+            ReportData reportData = TestUtility.PrepareApplicationReportData("ReportGenerator",
+               null, @".\Data\CurrentBCresults.json", "AED/applications/3/snapshots/6", "PreVersion 1.5.0 sprint 2 shot 2", "V-1.5.0_Sprint 2_2", currentDate,
+               null, null, null, null, null, null);
+            var component = new OmgTechnicalDebtTable();
+            WSConnection connection = new WSConnection
+            {
+                Url = "http://tests/CAST-RESTAPI/rest/",
+                Login = "admin",
+                Password = "cast",
+                IsActive = true,
+                Name = "Default"
+            };
+            reportData.SnapshotExplorer = new SnapshotBLLStub(connection, reportData.CurrentSnapshot);
+            Dictionary<string, string> config = new Dictionary<string, string>
+            {
+                {"ID", "AIP" }
+            };
+
+            var table = component.Content(reportData, config);
+
+            var expectedData = new List<string>();
+            expectedData.AddRange(new List<string> { "Name", "Value" });
+            expectedData.AddRange(new List<string> { "Technical Debt (Days)", "244.3" });
+            expectedData.AddRange(new List<string> { "Technical Debt Added (Days)", "244.3" });
+            expectedData.AddRange(new List<string> { "Technical Debt Removed (Days)", "0.0" });
+            TestUtility.AssertTableContent(table, expectedData, 2, 3);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\Data\CurrentBCresults.json", "Data")]
+        public void TestShortHeaders()
+        {
+            CastDate currentDate = new CastDate { Time = 1484953200000 };
+            ReportData reportData = TestUtility.PrepareApplicationReportData("ReportGenerator",
+                null, @".\Data\CurrentBCresults.json", "AED/applications/3/snapshots/6", "PreVersion 1.5.0 sprint 2 shot 2", "V-1.5.0_Sprint 2_2", currentDate,
+                null, null, null, null, null, null);
+            var component = new OmgTechnicalDebtTable();
+            WSConnection connection = new WSConnection
+            {
+                Url = "http://tests/CAST-RESTAPI/rest/",
+                Login = "admin",
+                Password = "cast",
+                IsActive = true,
+                Name = "Default"
+            };
+            reportData.SnapshotExplorer = new SnapshotBLLStub(connection, reportData.CurrentSnapshot);
+            Dictionary<string, string> config = new Dictionary<string, string>
+            {
+                {"ID", "AIP" },
+                {"HEADER","SHORT" }
+            };
+
+            var table = component.Content(reportData, config);
+
+            var expectedData = new List<string>();
+            expectedData.AddRange(new List<string> { "Name", "Value" });
+            expectedData.AddRange(new List<string> { "Debt", "244.3" });
+            expectedData.AddRange(new List<string> { "Debt Added", "244.3" });
+            expectedData.AddRange(new List<string> { "Debt Removed", "0.0" });
+            TestUtility.AssertTableContent(table, expectedData, 2, 3);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\Data\CurrentBCresults.json", "Data")]
+        public void TestPreviousNullContent()
+        {
+            CastDate currentDate = new CastDate { Time = 1484953200000 };
+            ReportData reportData = TestUtility.PrepareApplicationReportData("ReportGenerator",
+               null, @".\Data\CurrentBCresults.json", "AED/applications/3/snapshots/6", "PreVersion 1.5.0 sprint 2 shot 2", "V-1.5.0_Sprint 2_2", currentDate,
+               null, null, null, null, null, null);
+            var component = new OmgTechnicalDebtTable();
+            WSConnection connection = new WSConnection
+            {
+                Url = "http://tests/CAST-RESTAPI/rest/",
+                Login = "admin",
+                Password = "cast",
+                IsActive = true,
+                Name = "Default"
+            };
+            reportData.SnapshotExplorer = new SnapshotBLLStub(connection, reportData.CurrentSnapshot);
+            Dictionary<string, string> config = new Dictionary<string, string>
+            {
+                {"ID", "AIP" },
+                {"SNAPSHOT","PREVIOUS" }
+            };
+
+            var table = component.Content(reportData, config);
+
+            var expectedData = new List<string>();
+            expectedData.AddRange(new List<string> { "Name", "Value" });
+            expectedData.AddRange(new List<string> { "Technical Debt (Days)", "n/a" });
+            expectedData.AddRange(new List<string> { "Technical Debt Added (Days)", "n/a" });
+            expectedData.AddRange(new List<string> { "Technical Debt Removed (Days)", "n/a" });
+            TestUtility.AssertTableContent(table, expectedData, 2, 3);
+        }
+
+    }
+}
