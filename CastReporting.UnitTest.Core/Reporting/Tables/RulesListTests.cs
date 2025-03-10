fc78c8989a35a5fe726c58e5d9f0479abb77670a
CASTCORP\ABD <a.eteve@castsoftware.com>
2025-03-10 16:11:01 +0100

    REPORTGEN-1184: implement new option and update library templates

--- CastReporting.UnitTest.Core/Reporting/Tables/RulesListTests.cs
+++ CastReporting.UnitTest.Core/Reporting/Tables/RulesListTests.cs
@@ -70,6 +70,63 @@ namespace CastReporting.UnitTest.Reporting.Tables
             expectedData.AddRange(new List<string> { "", "45", "3.15", "Complexity - Algorithmic and Control Structure Complexity", "Avoid accessing data by using the position and length", "123", "1,234" });
             TestUtility.AssertTableContent(table, expectedData, 7, 3);
         }
+
+        [TestMethod]
+        [DeploymentItem(@"Data/RuleListBCTC.json", "Data")]
+        [DeploymentItem(@"Data/BaseQI60011.json", "Data")]
+        [DeploymentItem(@"Data/TechCrit61009NoViolations.json", "Data")]
+        public void TestContentNoViolationsTrue()
+        {
+            CastDate currentDate = new CastDate { Time = 1484953200000 };
+            ReportData reportData = TestUtility.PrepareApplicationReportData("ReportGenerator",
+               null, @"Data/RuleListBCTC.json", "AED/applications/3/snapshots/6", "PreVersion 1.5.0 sprint 2 shot 2", "V-1.5.0_Sprint 2_2", currentDate,
+               null, null, null, null, null, null);
+            reportData.RuleExplorer = new RuleBLLStub();
+            reportData.CurrentSnapshot = TestUtility.AddSameTechCritRulesViolations(reportData.CurrentSnapshot, @"Data/TechCrit61009NoViolations.json");
+
+            var component = new RulesList();
+            Dictionary<string, string> config = new Dictionary<string, string>
+            {
+                {"PAR","60011" }
+            };
+            var table = component.Content(reportData, config);
+
+            var expectedData = new List<string>();
+            expectedData.AddRange(new List<string> { "Criticality", "Weight", "Grade", "Technical Criterion", "Rule Name", "# Viol.", "Total Checks" });
+            expectedData.AddRange(new List<string> { "y", "9", "3.78", "Complexity - Algorithmic and Control Structure Complexity", "Action Mappings should have few forwards", "58", "3,117" });
+            expectedData.AddRange(new List<string> { "", "45", "3.15", "Complexity - Algorithmic and Control Structure Complexity", "Avoid accessing data by using the position and length", "123", "1,234" });
+            expectedData.AddRange(new List<string> { "", "72", "4.00", "Complexity - Algorithmic and Control Structure Complexity", "Avoid artifacts having recursive calls", "0", "4,767" });
+            TestUtility.AssertTableContent(table, expectedData, 7, 4);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"Data/RuleListBCTC.json", "Data")]
+        [DeploymentItem(@"Data/BaseQI60011.json", "Data")]
+        [DeploymentItem(@"Data/TechCrit61009NoViolations.json", "Data")]
+        public void TestContentNoViolationsFalse()
+        {
+            CastDate currentDate = new CastDate { Time = 1484953200000 };
+            ReportData reportData = TestUtility.PrepareApplicationReportData("ReportGenerator",
+               null, @"Data/RuleListBCTC.json", "AED/applications/3/snapshots/6", "PreVersion 1.5.0 sprint 2 shot 2", "V-1.5.0_Sprint 2_2", currentDate,
+               null, null, null, null, null, null);
+            reportData.RuleExplorer = new RuleBLLStub();
+            reportData.CurrentSnapshot = TestUtility.AddSameTechCritRulesViolations(reportData.CurrentSnapshot, @"Data/TechCrit61009NoViolations.json");
+
+            var component = new RulesList();
+            Dictionary<string, string> config = new Dictionary<string, string>
+            {
+                {"PAR","60011" },
+                {"NOVIOLATIONS","false" }
+            };
+            var table = component.Content(reportData, config);
+
+            var expectedData = new List<string>();
+            expectedData.AddRange(new List<string> { "Criticality", "Weight", "Grade", "Technical Criterion", "Rule Name", "# Viol.", "Total Checks" });
+            expectedData.AddRange(new List<string> { "y", "9", "3.78", "Complexity - Algorithmic and Control Structure Complexity", "Action Mappings should have few forwards", "58", "3,117" });
+            expectedData.AddRange(new List<string> { "", "45", "3.15", "Complexity - Algorithmic and Control Structure Complexity", "Avoid accessing data by using the position and length", "123", "1,234" });
+            TestUtility.AssertTableContent(table, expectedData, 7, 3);
+        }
+
     }
 }
 
