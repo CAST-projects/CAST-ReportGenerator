551c4d4bbe6d697af59494e73f1afb44b76bf323
CASTCORP\ABD <a.eteve@castsoftware.com>
2025-09-16 17:46:28 +0200

    REPORTGEN-1227: cleanup

--- Setup/install_dotnet_core.bat
+++ Setup/install_dotnet_core.bat
@@ -1,7 +1,6 @@
 @echo off
 SetLocal EnableDelayedExpansion
 
-rem set DOTNETCORENAME=dotnet-sdk-6.0.202-win-x64
 set DOTNETCORENAME=dotnet-sdk-8.0.414-win-x64
 set PACKPATH=%TEMP%\%DOTNETCORENAME%.exe 
 set LOGPATH=%TEMP%\%DOTNETCORENAME%.log
@@ -12,8 +11,6 @@ echo =================================================================
 echo Downloading .NET package ...
 echo =================================================================
 echo =================================================================
-rem C:\Windows\System32\curl.exe -o %PACKPATH% https://download.visualstudio.microsoft.com/download/pr/e4f4bbac-5660-45a9-8316-0ffc10765179/8ade57de09ce7f12d6411ed664f74eca/%DOTNETCORENAME%.exe
-rem C:\Windows\System32\curl.exe -o %PACKPATH% https://download.visualstudio.microsoft.com/download/pr/4b3b488c-9e69-4d60-bba2-79412b68d15d/b55f49a270c3413a6ea4b208f820515d/%DOTNETCORENAME%.exe
 C:\Windows\System32\curl.exe -o %PACKPATH% https://builds.dotnet.microsoft.com/dotnet/Sdk/8.0.414/%DOTNETCORENAME%.exe
 
 if not exist %PACKPATH% (
